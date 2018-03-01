32b01a42d0b9c2fac490768ec1f2d86100eee4d7
Kevin <omallek@gmail.com>
2018-03-01 10:38:59 -0600

    adds customer types in schema file

--- schema.js
+++ schema.js
@@ -7,7 +7,22 @@ const {
   GraphQLNonNull
 } = require('graphql')
 
+// Customer Type
+const CustomerType = new GraphQLObjectType({
+  name: 'Customer',
+  fields: () => ({
+    id: {type: GraphQLString},
+    name: {type: GraphQLString}
+  })
+})
 
+// Root Query
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  customer: {
+    type: CustomerType
+  }
+});
 module.exports = new GraphQLSchema({
 
 });
