40be43e73a65a3efb5ad6557e8e0102180e6adae
Kevin <omallek@gmail.com>
2018-03-03 12:12:33 -0600

    sets up hardcoded data to be queried

--- schema.js
+++ schema.js
@@ -20,23 +20,33 @@ const CustomerType = new GraphQLObjectType({
   name: 'Customer',
   fields: () => ({
     id: {type: GraphQLString},
-    name: {type: GraphQLString}
+    name: {type: GraphQLString},
+    email: {type: GraphQLString},
+    age: {type: GraphQLInt}
   })
-})
+});
 
 // Root Query
 const RootQuery = new GraphQLObjectType({
   name: 'RootQueryType',
-  customer: {
-    type: CustomerType,
-    args: {
-      id: {GraphQLString}
-    },
-    resolve(parentValue, args){
-      for (let i = 0; i < customers.length; i++) {
-        if (customers[i].id == args.id){
-          return customers[i];
+  fields: {
+    customer: {
+      type: CustomerType,
+      args: {
+        id: {type: GraphQLString}
+      },
+      resolve(parentValue, args){
+        for (let i = 0; i < customers.length; i++) {
+          if (customers[i].id == args.id){
+            return customers[i];
+          }
         }
+      }
+    },
+    customers: {
+      type: new GraphQLList(CustomerType),
+      resolve(parentValue, args){
+        return customers;
       }
     }
   }
