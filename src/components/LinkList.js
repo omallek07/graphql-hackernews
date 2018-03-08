28e8fbc699397e956e1d7d5c45020e2346ab084a
Kevin <omallek@gmail.com>
2018-03-08 10:33:25 -0600

    adds waiting and error handling middleware for feed query in linklist component

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -5,18 +5,16 @@ import gql from 'graphql-tag'
 
 class LinkList extends Component {
   render() {
-    const linksToRender = [
-      {
-        id: '1',
-        description: 'Prisma turns your database into a GraphQL API 😎 😎',
-        url: 'https://www.prismagraphql.com',
-      },
-      {
-        id: '2',
-        description: 'The best GraphQL client',
-        url: 'https://www.apollographql.com/docs/react/',
-      },
-    ]
+    //is true as long as request is still ongoing and the response has not been recieved
+    if (this.props.feedQuery && this.props.feedQuery.loading) {
+      return <div>Loading</div>
+    }
+    //in case the request fails, this will contain information about what went wrong
+    if (this.props.feedQuery && this.props.feedQuery.error) {
+      return <div>Error</div>
+    }
+    //actual data that was received from server. Has the links property which represents a list of Link elements
+    const linksToRender = this.props.feedQuery.feed.links
 
     return (
       <div>{linksToRender.map(link => <Link key={link.id} link={link} />)}</div>
