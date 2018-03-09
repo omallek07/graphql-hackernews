5bb708b05b5e29ebc027b0884832cb4fad837217
Kevin <omallek@gmail.com>
2018-03-09 15:35:05 -0600

    adds react router functionality and creates links to home and create

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './styles/index.css'
 import App from './components/App'
 import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter } from 'react-router-dom'
+
 
 // Imports required dependencies
 import { ApolloProvider } from 'react-apollo'
