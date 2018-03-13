38b3bd9618550b89ab6c03226c7dd0f0264da965
Kevin <omallek@gmail.com>
2018-03-13 08:20:59 -0500

    adds authentication middleware to indexjs

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './styles/index.css'
 import App from './components/App'
 import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter } from 'react-router-dom'
+import { AUTH_TOKEN } from './constants'
+import { ApolloLink } from 'apollo-client-preset'
 
 
 // Imports required dependencies
@@ -13,9 +15,23 @@ import { HttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 // Connects ApolloClient with GraphQL API
 const httpLink = new HttpLink({ uri: 'http://localhost:4000' })
+
+const middlewareAuthLink = new ApolloLink((operation, forward) => {
+  const token = localStorage.getItem(AUTH_TOKEN)
+  const authorizationHeader = token ? `Bearer ${token}` : null
+  operation.setContext({
+    headers: {
+      authorization: authorizationHeader
+    }
+  })
+  return forward(operation)
+})
+
+const httpLinkWithAuthToken = middlewareAuthLink.concat(httpLink)
+
 // Instantiate ApolloClient
 const client = new ApolloClient({
-  link: httpLink,
+  link: httpLinkWithAuthToken,
   cache: new InMemoryCache()
 })
 
