a6adfac0e0d771bba3cfae28861a5f2305f9e9b1
Kevin <omallek@gmail.com>
2018-03-22 15:01:52 -0500

    adds update function for cache

--- src/components/Link.js
+++ src/components/Link.js
@@ -41,6 +41,9 @@ class Link extends Component {
       variables: {
         linkId,
       },
+      update: (store, { data: { vote } }) => {
+        this.props.updateStoreAfterVote(store, vote, linkId)
+      },
     })
   }
 
